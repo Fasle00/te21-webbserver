77de5658a8beae82613dc136f9576814668e54d0
Fabian Sigfridsson <69erik355@gmail.com>
2023-12-12 10:02:26 +0100

    🐛

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ const indexRouter = require('./routes/index')
 const app = express()
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+  autoescape: true,
+  express: app,
 })
 
 const port = process.env.PORT || 3000
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 
-app.get("/", indexRouter)
+app.use((req, res, next) =>{
+  res.locals.url = req.originalUrl
+  next()
+})
+
+app.use('/', indexRouter)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
