606c667db278f2920d1b81edf8131a3e8f1ec374
Fabian Sigfridsson <69erik355@gmail.com>
2023-12-12 08:52:42 +0100

    🎨 added text

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", function (req, res) {
-  res.render('index.njk', { title: 'Hello from template' })
+  res.render('index.njk', { title: 'Hello from template', text: 'text' })
 })
 
 module.exports = router
